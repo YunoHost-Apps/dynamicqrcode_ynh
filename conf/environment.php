4db8cbcbc74c9af2befeda2671de5123f412161d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-12-17 22:26:18 +0100

    first commit

---
+++ conf/environment.php
@@ -0,0 +1,16 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| INSTALLATION WITHOUT CONTAINER
+|--------------------------------------------------------------------------
+ */
+
+define('DATABASE_HOST', "localhost");
+define('DATABASE_PORT', "3306");
+define('DATABASE_NAME', "__DB_NAME__");
+define('DATABASE_USER', "__DB_USER__");
+define('DATABASE_PASSWORD', "__DB_PWD__");
+define('DATABASE_PREFIX', "qr_"); // This must have a trailing underscore. Example: qr_
+define('DATABASE_CHARSET', "utf8");
+
+?>
